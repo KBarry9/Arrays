b0183ce09316d0f03280923d2b71a2f58deed05d
Kelly Barry <kleemuir@gmail.com>
2022-09-21 10:04:55 -0600

    Add Array changes

--- arrays.js
+++ arrays.js
@@ -1,3 +1,12 @@
 var dogNames = ["Gypsy", "Zuko", "Xena", "Rocky", "Sadie"]
 var Ages = [6, 4, 5, 7, 8]
 var pottyTrained = [true, false, true, false, true]
+
+// This is adding the name of Luna to the beginning of the array
+dogNames.unshift("Luna")
+
+//This is removing the first age (our index 0)
+Ages.shift()
+
+//This is removing our last value
+pottyTrained.pop()
